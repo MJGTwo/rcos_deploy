cc03e650702a696fc30cc818e6d409675bdf3d1e
MJGTwo <gardner.j.michael@gmail.com>
2023-06-28 12:54:56 -0400

    javascript server readme

--- javascript/index.js
+++ javascript/index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('js says "Hello World!"')
-})
+// app.get('/', (req, response) => {
+//   response.send('js says "Hello World!"')
+// })
+
+function hello(request, response) {
+  response.send('Hello Brendan')
+}
+app.get('/', hello)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
